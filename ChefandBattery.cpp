e35638aa3111286f51c9bcec25a26a313c800f97
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2024-03-23 13:58:32 +0530

    ChefandBattery.cpp

---
+++ ChefandBattery.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    int ans=0;
+	    while(n != 50){
+	        if(n<50){
+	            n+=2;
+	        }
+	        else if (n>50){
+	            n=(n-3);
+	        }
+	        ans += 1;
+	    }
+	    cout<<ans<<endl;
+	}
+    return 0;
+}
