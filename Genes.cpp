bb48b6e156fe582b16f763a08f2f6184d378ce0c
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2024-03-23 14:02:04 +0530

    Genes.cpp

---
+++ Genes.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string a,b;
+	cin>>a>>b;
+	
+	if((a=="R" && b=="B") || (b=="R" && a=="B")){
+	    cout<<"R"<<endl;
+	}
+	else if((a=="G" && b=="B") || (b=="G" && a=="B")){
+	    cout<<"B"<<endl;
+	}
+	else if((a=="G" && b=="R") || (b=="G" && a=="R")){
+	    cout<<"R"<<endl;
+	}
+	else if((a=="G" && b=="G")  || (b=="G" && a=="G")){
+	    cout<<"G"<<endl;
+	}
+	else if((a=="R" && b=="R") || (b=="R" && a=="R")){
+	    cout<<"R"<<endl;
+	}
+	else if((a=="B" && b=="B") || (b=="B" && a=="B")){
+	    cout<<"B"<<endl;
+	}
+	
+
+}
