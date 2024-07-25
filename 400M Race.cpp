06866874b2141c61f5a38b5488e8981fdb847608
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2024-07-25 21:15:28 +0530

    400M Race.cpp

---
+++ 400M Race.cpp
@@ -0,0 +1,23 @@
+//Difficulty:  548 
+//400M Race
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+
+        // Since higher speed means lower time, we need to find the minimum time
+        if (x < y && x < z) 
+            cout << "ALICE" << endl;
+        else if (y < x && y < z) 
+            cout << "BOB" << endl;
+        else 
+            cout << "CHARLIE" << endl;
+    }
+    return 0;
+}
