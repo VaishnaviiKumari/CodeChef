2faffca54f18ffad44d5113209dd5160bcf10f44
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2025-03-27 21:03:54 +0530

    Car Choice.cpp

--- Car Choice.cpp
+++ Car Choice.cpp
@@ -10,7 +10,7 @@ int main() {
 	    cin>>x1>>x2>>y1>>y2; 
 	     
 	    float a = y1/x1;
-	    float b = y2/x2;
+	    float b = y2/x2; 
 	    
 	    if(a<b) cout<<-1<<endl;
 	    else if (b<a) cout<<1<<endl;
